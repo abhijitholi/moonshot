5f915809371e3199b35b8b0399db2c7c950c6349
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-07-14 19:38:22 +0530

    all file remove main page inside

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,9 +1,9 @@
 
 
-export default function BlogPostDetails(params) {
-    return (
-        <div>
-            <h1>BlogPostDetails{params.params.id}</h1>
-        </div>
-    );
-}
+// export default function BlogPostDetails(params) {
+//     return (
+//         <div>
+//             <h1>BlogPostDetails{params.params.id}</h1>
+//         </div>
+//     );
+// }

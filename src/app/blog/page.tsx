11a18bdea84b1d84e66553fd0fb9831d11c46283
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-07-15 10:25:42 +0530

    some add data

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,33 +1,33 @@
 'use client'
 import getDomain from "../lib/getDomain";
 
-// async function getData(){
-//     const domain = getDomain();
-//     const endpoint = `${domain}/api/post`;
-//     const response = await fetch(endpoint);
+async function getData(){
+    const domain = getDomain();
+    const endpoint = `${domain}/api/post`;
+    const response = await fetch(endpoint);
     
-//     if(!response.ok){
-//         throw new Error('faild to fetch data');
-//     }
+    if(!response.ok){
+        throw new Error('faild to fetch data');
+    }
 
-//     const data = await response.json();
-//     return data;
+    const data = await response.json();
+    return data;
 
-//     //return{items:[]}
-// }
+    //return{items:[]}
+}
 
 export default async function BlogPost() {
-    //const data = await getData();
+    const data = await getData();
     //const items = data && data.items ? [...data.items] : []; 
     return (
         <>
           <h1>Blog</h1>
 
-          {/* <ul>
+          <ul>
             {data && data.items && data.items.map((item: any) => (
               <li key={item.id}>{item.name}</li>
             ))}
-          </ul>    */}
+          </ul>   
     
         </>
       );

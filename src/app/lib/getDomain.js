ee4c0397e8ca11de323b79bfc08440cc1e240897
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-07-15 20:08:49 +0530

    Update getDomain.js

--- src/app/lib/getDomain.js
+++ src/app/lib/getDomain.js
@@ -1,5 +1,10 @@
-export default  function getDomain() {
+export default function getDomain() {
+    // Determine the protocol based on the environment
     const protocol = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "https" : "http";
+    
+    // Determine the domain based on the environment variable, defaulting to localhost for development
     const domain = process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL : "localhost:3000";
-    return `${protocol}://${domain}`; 
+    
+    // Return the full domain URL
+    return `${protocol}://${domain}`;
 }

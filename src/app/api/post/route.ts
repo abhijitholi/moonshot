bb4179dc93281ac9885005a674eeaed228609f67
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-07-15 16:44:24 +0530

    url all good

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,10 +1,6 @@
-// import { NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-// export async function GET(request : Request) {
-//   return NextResponse.json({ items: [{ id:1, name: 'kajal' },{ id:2, name: 'Abhishek' },{ id:3, name: 'Anshu' }] });
-// } 
+export async function GET(request : Request) {
+  return NextResponse.json({ items: [{ id:1, name: 'kajal' },{ id:2, name: 'Abhishek' },{ id:3, name: 'Anshu' }] });
+} 
 
-
-export async function GET(request: Request) {
-  return Response.json({ data: 'Next.js' });
-}

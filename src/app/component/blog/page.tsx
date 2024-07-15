e5ced92fe539afcb373fe866b717f777e222a29f
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-07-15 14:08:49 +0530

    lest update

--- src/app/component/blog/page.tsx
+++ src/app/component/blog/page.tsx
@@ -30,19 +30,45 @@ return res.json();
 
 }
 
+
+interface Item {
+  title: string;
+  // add other properties if needed
+}
+
 export default async function BlogPost() {
-    const data = await getData();
-    const stringData = JSON.stringify(data);
-    const items = stringData && stringData ? JSON.parse(stringData) : []; 
-    console.log(stringData)
-    return ( 
-        <>
-          <h1>Blog</h1>
-          <p>Posts:</p>
-            {items && items.map((item,idx)=>{
-              return <li key={`post-${idx}`}>{item.title}</li>
-            })}
+  const data = await getData();
+  const stringData = JSON.stringify(data);
+  const items: Item[] = stringData ? JSON.parse(stringData) : [];
+  //console.log(stringData);
+  return (
+    <>
+      <h1>Blog</h1>
+      <p>Posts:</p>
+      <ul>
+        {items.map((item: Item, idx: number) => {
+          return <li key={`post-${idx}`}>{item.title}</li>;
+        })}
+      </ul>
+    </>
+  );
+}
+
+// export default async function BlogPost() {
+//     const data = await getData();
+//     const stringData = JSON.stringify(data);
+//     const items = stringData && stringData ? JSON.parse(stringData) : []; 
+//     console.log(stringData)
+//     return ( 
+//         <>
+//           <h1>Blog</h1>
+//           <p>Posts:</p>
+//           <ul>
+//       {items && items.map((item, idx) => {
+//         return <li key={`post-${idx}`}>{item.title}</li>
+//       })}
+//     </ul>
       
-        </>
-      );
-    }
+//         </>
+//       );
+//     }

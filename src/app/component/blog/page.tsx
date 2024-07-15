c66ebdaad08ed0f93ee2ced82789821dbf132781
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-07-15 19:12:01 +0530

    Update page.tsx

--- src/app/component/blog/page.tsx
+++ src/app/component/blog/page.tsx
@@ -1,44 +1,44 @@
 // 'use client'
-// import getDomain from "@/app/lib/getDomain";
+import getDomain from "@/app/lib/getDomain";
 
-// async function getData(){
-//     const domain = getDomain();
-//     const endpoint = `${domain}/api/post`;
+async function getData(){
+    const domain = getDomain();
+    const endpoint = `${domain}/api/post`;
     
-//     const res = await fetch(endpoint , { cache: 'force-cache' });
-//     if(!res.ok){
-//         throw new Error('faild to fetch data');  
-//     } 
-// console.log(endpoint) 
+    const res = await fetch(endpoint , { cache: 'force-cache' });
+    if(!res.ok){
+        throw new Error('faild to fetch data');  
+    } 
+console.log(endpoint) 
 
 
-// if(res.headers.get("content-type")!=="application/json"){
+if(res.headers.get("content-type")!=="application/json"){
   
-//   return{items:[]};
-// }
+  return{items:[]};
+}
 
  
-// return res.json(); 
+return res.json(); 
 
-// }
+}
 
 
 
 
 export default async function BlogPost() {
-  // const data = await getData();
-  // const stringData = JSON.stringify(data);
-  // const items = stringData && stringData ? [...data.items] : []; 
-  //console.log(stringData)
+  const data = await getData();
+  const stringData = JSON.stringify(data);
+  const items = stringData && stringData ? [...data.items] : []; 
+  console.log(stringData)
   return ( 
       <>
         <h1>Blog</h1>
         <p>Posts:</p>
-        {/* <ul>
+        <ul>
           {items && items.map((item, idx) => {
             return <li key={`post-${idx}`}>{item.name}</li>
           })}
-        </ul> */}
+        </ul>
       </>
   );
 }

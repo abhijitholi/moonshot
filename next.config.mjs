a0cb8475b39ee701f9f13f3c87977bbe461df09b
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-07-15 16:07:04 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+import dns from "dns";
+
+// Set DNS resolution to prefer IPv4 over IPv6
+dns.setDefaultResultOrder("ipv4first");
+
+// Rest of your application code
+
+
 export default nextConfig;

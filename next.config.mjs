391b013a515e23f2b646150187c01f710c08671c
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-07-15 16:20:45 +0530

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-import dns from "dns";
-
-// Set DNS resolution to prefer IPv4 over IPv6
-dns.setDefaultResultOrder("ipv4first");
-
-// Rest of your application code
-
-
 export default nextConfig;
--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request : Request) {
   return NextResponse.json({ items: [{ id:1, name: 'kajal' },{ id:2, name: 'Abhishek' },{ id:3, name: 'Anshu' }] });
 } 

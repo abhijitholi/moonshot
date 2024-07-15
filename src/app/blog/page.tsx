384c10c9752413dcd202d5333fc6130f3f675ab8
abhijitholi <80149300+abhijitholi@users.noreply.github.com>
2024-07-15 11:10:57 +0530

    reupdate

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,35 +8,35 @@ import getDomain from "../lib/getDomain";
 
 
 
-// async function getData(){
-//     const domain = getDomain();
-//     const endpoint = `${domain}/api/post`;
-//     const response = await fetch(endpoint);
+async function getData(){
+    const domain = getDomain();
+    const endpoint = `${domain}/api/post`;
+    const response = await fetch(endpoint);
     
-//     if(!response.ok){
-//         throw new Error('faild to fetch data');
-//     }
+    if(!response.ok){
+        throw new Error('faild to fetch data');
+    }
 
-// if(response.headers.get("content-type")!=="application/json"){
+if(response.headers.get("content-type")!=="application/json"){
   
-//   return{items:[]}
-// }
+  return{items:[]}
+}
 
-// const data = await response.json();
-// return data;
+ 
+return response.json();
 
-// }
+}
 
 export default async function BlogPost() {
-    // const data = await getData();
-    // const items = data && data.items ? [...data.items] : []; 
+    const data = await getData();
+    const items = data && data.items ? [...data.items] : []; 
     return ( 
         <>
           <h1>Blog</h1>
           <p>Posts:</p>
-            {/* {items && items.map((item,idx)=>{
+            {items && items.map((item,idx)=>{
               return <li key={`post-${idx}`}>{item.name}</li>
-            })} */}
+            })}
       
         </>
       );

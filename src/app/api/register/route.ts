5221a5bb4daca9b79416c318aefd74833a0779b9
abhijitholi <jitholi83@gmail.com>
2024-07-17 22:25:25 +0530

    Update route.ts

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,62 +1,58 @@
-import { hash } from 'bcrypt';
-import { sql } from '@vercel/postgres';
-import { NextRequest, NextResponse } from 'next/server';
-// Otp 
-import EmailTemplate  from '@/app/api/resend/email-template';
-import { Resend } from 'resend';
+import { hash } from "bcrypt";
+import { sql } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
+// Otp
+import EmailTemplate from "@/app/api/resend/email-template";
+import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-
-
-export async function POST(request:NextRequest){
-
-
+export async function POST(request: NextRequest) {
   // OTP function
   function generateAlphanumericOTP(length: number): string {
-    let otp = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    
+    let otp = "";
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
     for (let i = 0; i < length; i++) {
-        otp += characters[Math.floor(Math.random() * characters.length)];
+      otp += characters[Math.floor(Math.random() * characters.length)];
     }
-    
+
     return otp;
-}
-const OTP = generateAlphanumericOTP(6);
-  try{
-    const reqBody =await request.json()
-    const {username,email,password} = reqBody;
+  }
+  const OTP = generateAlphanumericOTP(6);
+  try {
+    const reqBody = await request.json();
+    const { username, email, password } = reqBody;
     try {
       const { data, error } = await resend.emails.send({
-        from: 'Moonshot e-commerce <moonshot@resend.dev>',
+        from: "Moonshot e-commerce <moonshot@resend.dev>",
         to: [`${email}`],
-        subject: 'Moonshot e-commerce Email Verifaction Code', 
-        react: EmailTemplate({ username:`${username}`, otp:OTP }),
+        subject: "Moonshot e-commerce Email Verification Code",
+        text: "", // Add this line
+        react: EmailTemplate({ username: `${username}`, otp: OTP }),
       });
-  
-      if (error) { 
+
+      if (error) {
         return Response.json({ error }, { status: 500 });
       }
-  
+
       return Response.json(data);
     } catch (error) {
       return Response.json({ error }, { status: 500 });
     }
-    
+
     await sql`
     INSERT INTO users (username, email, password)
     VALUES (${username}, ${email}, ${password} ${OTP})
     `;
-    
+
     // otp Start **********************************
-  
-  
-  return NextResponse.json({message:`User registered successfully`},
-  {status:200}
-  )
- }catch(error:any){
-  return NextResponse.json({error:error.message},
-    {status:500}
-  )
- }
-} 
+
+    return NextResponse.json(
+      { message: `User registered successfully` },
+      { status: 200 }
+    );
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
